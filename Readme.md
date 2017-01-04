08b1bffedcb86facd9ec6d96a199ac5e6b8b395f
malex <alexmaximovj@gmail.com>
2017-01-04 18:02:49 +0200

    added gulp: connect and autoprefix

--- Readme.md
+++ Readme.md
@@ -39,3 +39,46 @@ gulp.task('watch', function () {
 'css/*.css' -> path to css files
 ['css'] -> exist task
 </pre>
+
+**Step#4**
+1. Set plugin: gulp-autoprefixer ( caniuse.com)
+2. FIX gulpfile.js
+<pre>
+  .pipe(autoprefixer({
+            browsers: ['last 15 versions'],
+            cascade: false
+        }))
+</pre>
+
+**Step#5**
+1. Set plugin: 
+* gulp-connect: 
+(**npm install --save-dev gulp-connect**) 
+* plugin gulp-livereload:
+ (**npm install --save-dev gulp-livereload**)
+
+2. FIX gulpfile.js
+<pre>
+// Connect
+gulp.task('connectDev', function () {
+    connect.server({
+        name: 'Dev App',
+        root: ['app'],
+        port: 8000,
+        livereload: true
+    });
+});
+
+// HTML
+gulp.task('html', function () {
+    return gulp.src('app/*.html')
+        .pipe(connect.reload());
+});
+
+// CSS
+.pipe(connect.reload());
+
+//WATCH
+ gulp.watch(['./app/*.html'], ['html']);
+</pre>
+
--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,44 @@
 var gulp = require('gulp');
 var concatCss = require('gulp-concat-css');
 var minifyCSS = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var connect = require('gulp-connect');
 
 // CSS
 gulp.task('css', function () {
     return gulp.src('css/**/*.css')
         .pipe(concatCss("bundle.min.css"))
+        .pipe(autoprefixer({
+            browsers: ['last 15 versions'],
+            cascade: false
+        }))
         .pipe(minifyCSS())
-        .pipe(gulp.dest('app/style'));
+        .pipe(gulp.dest('app/style'))
+        .pipe(connect.reload());
 });
 
-// WATCH
-gulp.task('watch', function () {
-    return gulp.watch('css/*.css', ['css'])
+// HTML
+gulp.task('html', function () {
+    return gulp.src('app/*.html')
+        .pipe(connect.reload());
 });
+
+// Watch
+gulp.task('watch', function () {
+    gulp.watch('css/*.css', ['css']);
+    gulp.watch(['app/*.html'], ['html']);
+});
+
+// Connect
+gulp.task('connect', function () {
+    connect.server({
+        name: 'Dev App',
+        root: ['app'],
+        port: 8000,
+        livereload: true
+    });
+});
+
+// Build dev app
+gulp.task('server', ['connect', 'html', 'css', 'watch']);
+
